68673181e0024adf2a69b61d5a07be7beada65a1
lev <leva22.08.01@inbox.ru>
2022-10-07 16:22:24 +0300

    [fix] change if/else to try/except

--- safe_evaluation/expressions.py
+++ safe_evaluation/expressions.py
@@ -298,10 +298,10 @@ def _get_variable(var, df, local) -> pd.Series:
         if var[0] == TypeOfCommand.VALUE:
             variable = var[1]
         elif var[0] == TypeOfCommand.COLUMN:
-            if var[1] in df.columns:
+            try:
                 variable = df[var[1]]
-            else:
-                raise Exception(f"The input DataFrame doesn't contain \"{var[1]}\" column")
+            except KeyError:
+                raise KeyError(f"The input DataFrame doesn't contain \"{var[1]}\" column")
         elif var[0] == TypeOfCommand.VARIABLE:
             if local and var[1] in local:
                 variable = local[var[1]]
